c7a7a00e0fdf4d880ad49954246486e4fa95c795
Polxpro <girbauiguerreropol@protonmail.com>
2026-02-10 18:45:21 +0100

    app.py update

--- static/app.py
+++ static/app.py
@@ -1,1 +1,10 @@
-from flask, 
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/')
+def index():
+    return render_template('/templates/Index.html')
